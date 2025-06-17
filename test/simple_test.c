a7436f50ada037d0b2decfd36ff36ea64ef16d31
zealot717 <findtanmay17@gmail.com>
2025-06-17 14:20:44 +0530

    No Readme

---
+++ test/simple_test.c
@@ -0,0 +1,21 @@
+#include "/home/bala/cd-lab/runtime/runtime.h"
+
+int square(int n) {
+    
+
+    int sum;
+    if(n>2)
+     return 0;
+    for(int i=0;i<2000;i++)
+     sum+=0;
+    return sum;
+
+
+    
+     
+}
+
+int main() {
+    int r = square(5);
+    return 0;
+}
